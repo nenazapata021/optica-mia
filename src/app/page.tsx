8c69b17e270ef3dc5298fd3d373eb1e3b77cadfc
Edna Castañeda <nenazapata021@gmail.com>
2026-07-27 11:49:03 -0500

    Pequeño avance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ import Contacto from "./contacto/contacto";
 import Testimonios from "./testimonios/Testimonios";
 
 const productosInicio = [
-    { id: 'p1',  name: 'Montura',         price: 120000, image: foto1SinFondo,  categoria: 'mujer',  color: 'Transparente', descripcion: '' },
+    { id: 'p22', name: 'Montura',         price: 120000, image: foto1SinFondo,  categoria: 'mujer',  color: 'Transparente', descripcion: '' },
     { id: 'p1',  name: 'Montura Clásica',         price: 120000, image: foto1,  categoria: 'mujer',  color: 'Transparente', descripcion: '' },
     { id: 'p2',  name: 'Montura Rosa',            price: 150000, image: foto2,  categoria: 'mujer',  color: 'Rosa',         descripcion: '' },
     { id: 'p3',  name: 'Montura Dorada',          price: 180000, image: foto3,  categoria: 'mujer',  color: 'Dorado',       descripcion: '' },
